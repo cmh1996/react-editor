d8b980efc6075d27d12fb9b842e85436877e2c01
cmh1996 <752380178@qq.com>
2018-10-28 17:36:06 +0800

    fix bugs

--- src/App.js
+++ src/App.js
@@ -5,66 +5,165 @@ class App extends Component {
   constructor(){
     super();
     this.state = {
-      operateList:[{
-        text:"加粗"
-      },{
-        text:"斜体"
-      },{
-        text:"下划线"
-      }],
-      curMode:""
+      operateList:{
+        "bold":false,
+        "underline":false,
+        "italic":false,
+        "undo":false,
+        "redo":false
+      }
     };
+    this.needToSwitchBtn = ["bold","underline","italic"];
+    this.rangePos = null;
+    this.editorContent = React.createRef();
+    this.editorBox = React.createRef();
   }
 
   componentDidMount(){
-    this.refs.editorContent.focus();
+    //文本框自动聚焦
+    this.editorContent.current.focus();
+    
+    document.addEventListener("selectionchange",this.debounce(this.selectionChangeHandle,500,true),false);
+    this.editorContent.current.addEventListener("mouseup",this.checkIfMultiSelect,false);
   }
 
-  switchMode = (e)=>{
-    //用execCommand必须先focus，不然不生效
-    this.refs.editorContent.focus();
+  //检查是否是框选，是的话mouseup时触发selectionChangeHandle
+  checkIfMultiSelect = ()=>{
+    const selection = window.getSelection();
+    if(!selection.isCollapsed){
+      this.selectionChangeHandle();
+    }
+  }
 
+  //当光标变化时，按钮选中状态也要相应改变
+  selectionChangeHandle = ()=>{
+    const selection = window.getSelection();
+    const range = selection.getRangeAt(0);
+    //如果是点击按钮的，就跳过判断
+    if(range.commonAncestorContainer.parentElement.className.indexOf("editor-header-btn")!==-1){
+      return;
+    }
+    
+    const oList = Object.assign({},this.state.operateList);
+    oList["bold"] = document.queryCommandState("bold") ? true : false;
+    oList["italic"] = document.queryCommandState("italic") ? true : false;
+    oList["underline"] = document.queryCommandState("underline") ? true : false;
     this.setState({
-      curMode:e.target.innerText
-    },()=>{
-      switch(this.state.curMode){
-        case "加粗":
-          document.execCommand('bold', false, null);
-          break;
-        case "斜体":
-          document.execCommand('italic', false, null);
-          break;
-        case "下划线":
-          document.execCommand('underline', false, null);
-          break;
-        default:
-          console.log("sth wrong");
-      }
-    })
+      operateList:oList
+    });
   }
+
+  //失去焦点时保存光标信息
+  saveRange = (e)=>{
+    const selection = window.getSelection();
+    if (!selection.rangeCount) return;
+    this.rangePos = selection.getRangeAt(0);
+  }
+
+  //设置光标位置
+  setRangePos = ()=>{
+    const selection = window.getSelection();
+    if (this.rangePos!=null) {
+      try {   
+        selection.removeAllRanges();
+      } catch (ex) {   
+        document.body.createTextRange().select();   
+        document.selection.empty();   
+      }  
+      selection.addRange(this.rangePos);
+    }
+  }
+
+  //切换模式
+  switchMode = (e)=>{
+    console.log(document.queryCommandEnabled("undo"))
+    //用execCommand必须先focus，不然不生效
+    this.setRangePos();
+
+    const curMode = e.target.innerText;
+    if(this.needToSwitchBtn.includes(curMode)){
+      const oList = Object.assign({},this.state.operateList);
+      oList[curMode] = !oList[curMode];
+      this.setState({
+        operateList:oList
+      });
+    }
+    switch(curMode){
+      case "bold":
+        document.execCommand('bold', false, null);
+        break;
+      case "italic":
+        document.execCommand('italic', false, null);
+        break;
+      case "underline":
+        document.execCommand('underline', false, null);
+        break;
+      case "undo":
+        document.execCommand('undo', false, null);
+        break;
+      case "redo":
+        document.execCommand('redo', false, null);
+        break;
+      default:
+        console.log("sth wrong");
+    }
+  }
+
+  //防抖
+  debounce = (func, wait, immediate)=>{
+    var timeout, result;
+    var debounced = function () {
+        var context = this;
+        var args = arguments;
+        if (timeout) clearTimeout(timeout);
+        if (immediate) {
+            // 如果已经执行过，不再执行
+            var callNow = !timeout;
+            timeout = setTimeout(function(){
+                timeout = null;
+            }, wait)
+            if (callNow){
+              result = func.apply(context, args);
+            }
+        }
+        else {
+            timeout = setTimeout(function(){
+                func.apply(context, args)
+            }, wait);
+        }
+        return result;
+    };
+    debounced.cancel = function() {
+        clearTimeout(timeout);
+        timeout = null;
+    };
+    return debounced;
+  }
+
 
   render() {
     return (
       <div className="App">
-        <div className="editor-box">
+        <div className="editor-box" ref={this.editorBox}>
           <ul className="editor-header">
             {
-              this.state.operateList.map((item,index)=>{
+              Object.keys(this.state.operateList).map((item,index)=>{
                 return (
-                  <li className={this.state.curMode===item.text?"editor-header-btn selected":"editor-header-btn"} 
+                  <li className={this.state.operateList[item]?"editor-header-btn selected":"editor-header-btn"} 
                       key={index}
                       onClick={this.switchMode}
                   >
-                    {item.text}
+                    {item}
                   </li>
                 )
               })
             }
           </ul>
           <div 
-            ref="editorContent" 
+            ref={this.editorContent} 
             className="editor-content" 
             contentEditable
+            onBlur={this.saveRange}
           >
           </div>
         </div>
